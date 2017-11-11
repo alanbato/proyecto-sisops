34ba33431790e97b5332545c6712e8cbdc5830b9
Enrique Barragan <ebarragan1997@gmail.com>
2017-11-11 13:34:35 -0600

    new version

--- sjf.py
+++ sjf.py
@@ -1,74 +1,83 @@
-class pcb:
-    pID = None
-    bTime = 0  # Burst time
-    aTime = 0  # Arrival time
-    wTime = 0  # Waiting time
-    xTime = 0  # Executing time
-    fTime = 0  # Completion time
+import copy
+
+
+class Process:
+
+    def __init__(self, pid, arrival_time, cpu_time, priority, io_operations):
+        self.pid = pid
+        self.arrival_time = arrival_time
+        self.cpu_time = cpu_time
+        self.remaining_time = cpu_time
+        self.priority = priority
+        self.io_operations = io_operations
+        self.io_operation_duration = 0
+        self.waiting_time = 0
+        self.execution_time = 0
+        self.completion_time = 0
 
 
 def comp2(g, h):
-    x = cmp(g.bTime, h.bTime)
+    x = cmp(g.cpu_time, h.cpu_time)
     if x == 0:
-        x = cmp(g.pID, h.pID)
+        x = cmp(g.pid, h.pid)
     return x
 
 
-def updateque(L, i):
-    global master
+def updateque(ready, runtime, master):
     for x in master[:]:
-        if x.aTime <= i:
-            L.append(x)
+        if x.arrival_time <= runtime:
+            ready.append(x)
             j = master.index(x)
             del master[j]
-    return L
+    return ready
 
 
-def importFi(fileName):
-    temp = pcb()
-    holder = []
-    f = open(fileName, 'r')
-    for line in f:
+def block_status(blocked, ready):
+    for process in blocked:
+        process.io_operation_duration -= 1
+        if process.io_operation_duration == 0:
+            ready.append(process)
 
 
-def sjf():
-    global processTotal
-    print "\nSJF schduler"
+def sjf(process_total, master):
+    print("\nSJF schduler")
     runtime = 0
-    allDone = False
+    all_done = False
     ready = []
     done = []
-    ready = updateque(ready, runtime)
-    while not allDone:
-        ready.sort(cmp=comp2)
-        print runtime, ready[0].pID
-        while ready[0].xTime != ready[0].bTime:
+    blocked = []
+    ready = updateque(ready, runtime, master)  # Get process that were submitted
+    while not all_done:
+        ready.sort(cmp=comp2)  # Sort shortest burst time first
+        print(runtime, ready[0].pid)
+
+        while ready[0].execution_time != ready[0].cpu_time:  # Execute process until it finishes
+            if ready[0].execution_time in ready[0].io_operations:
+                ready[0].io_operation_duration = ready[0].io_operations[ready[0].execution_time]
+                ready[0].execution_time += 1
+                ready[0].remaining_time -= 1
+                blocked.append(ready[0])
+                del ready[0] 
+            else:
+                ready[0].execution_time += 1
+                ready[0].remaining_time -= 1
             runtime += 1
-            ready[0].xTime += 1
+            block_status(blocked, ready)
+            # Get process that were submitted
+            ready = updateque(ready, runtime, master)
 
-            for index in range(len(ready)):
-                if index != 0:
-                    ready[index].wTime += 1
-            ready = updateque(ready, runtime)
-        ready[0].fTime = runtime
-        done.append(ready[0])
-        del ready[0]
-        if len(done) == processTotal:
-            allDone = True
+        ready[0].completion_time = runtime  # Record the time process finished
+        done.append(ready[0])  # Put process in the done queue
+        del ready[0]  # Take process of the ready queue
+        if len(done) == process_total:
+            all_done = True
     print runtime, "Complete"
-    printSummary(done)
+    return done
 
 
-def main(argv):
-    global master
-    global processTotal
-    fileInput = importFi(argv)
-    processTotal = len(fileInput)
+def main(processes):
+    process_total = len(processes)
     master = []
-    master = copy.deepcopy(fileInput)
+    master = copy.deepcopy(processes)
 
-    sjf()
-
-
-if __name__ == "__main__":
-    main(sys.argv[1])
+    sjf(process_total, master)
